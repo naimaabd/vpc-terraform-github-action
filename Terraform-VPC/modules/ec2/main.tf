1bbe06447af56cc3c1eb2d7ecf1f5aaf6d6b4d31
Naima Abdulle <naimaabdulle@outlook.com>
2024-04-03 20:55:23 +0200

    Adding Terraform and Github Action Files

---
+++ Terraform-VPC/modules/ec2/main.tf
@@ -0,0 +1,76 @@
+resource "aws_instance" "web" {
+  count                       = length(var.ec2_names)
+  ami                         = data.aws_ami.amazon-2.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [var.sg_id]
+  subnet_id                   = var.subnets[count.index]
+  availability_zone         = data.aws_availability_zones.available.names[count.index]
+  user_data = <<EOF
+#!/bin/bash
+
+    sudo yum update -y
+    sudo yum install -y nginx
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+
+    cat <<HTML > /usr/share/nginx/html/index.html
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Awesome Website</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f0f0f0;
+                margin: 0;
+                padding: 0;
+            }
+            .container {
+                max-width: 800px;
+                margin: 50px auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 10px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                text-align: center;
+            }
+            h1 {
+                color: #333;
+            }
+            p {
+                color: #666;
+            }
+            .button {
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #007bff;
+                color: #fff;
+                text-decoration: none;
+                border-radius: 5px;
+                transition: background-color 0.3s ease;
+            }
+            .button:hover {
+                background-color: #0056b3;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome to My Awesome Website!</h1>
+            <p>This is a simple and visually appealing static website served by Nginx.</p>
+            <a class="button" href="#">Learn More</a>
+        </div>
+    </body>
+    </html>
+    HTML
+
+    sudo systemctl restart nginx
+  EOF
+
+  tags = {
+    Name = var.ec2_names[count.index]
+  }
+}
