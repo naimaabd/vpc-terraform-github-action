8e17fa3b2ae6558b5293477b5235bbc1d6d58970
Naima Abdulle <naimaabdulle@outlook.com>
2024-04-03 21:32:09 +0200

    Fixed dublicates that was causing validation issue

--- Terraform-VPC/variables.tf
+++ Terraform-VPC/variables.tf
@@ -4,7 +4,28 @@ variable "vpc_cidr" {
 }
 
 variable "subnet_cidr" {
-  description = "subnet CIDRS"
+  description = "Subnet CIDRs"
   type        = list(string)
+}
 
+variable "subnet_names" {
+  description = "Subnet names"
+  type        = list(string)
+  default     = ["PublicSubnet1", "PublicSubnet2"]
+}
+
+variable "ec2_names" {
+  description = "EC2 names"
+  type        = list(string)
+  default     = ["WebServer1", "WebServer2"]
+}
+
+variable "vpc_id" {
+  description = "VPC ID for ALB"
+  type        = string
+}
+
+variable "instances" {
+  description = "Instance IDs for Target Group Attachment"
+  type        = list(string)
 }
